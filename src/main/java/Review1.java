4cd2eba41884c881134974f277ba66c18ca59211
yuyen <yu-yen.shen@student.tdsb.on.ca>
2025-02-04 20:14:32 +0000

    yuyen

--- src/main/java/Review1.java
+++ src/main/java/Review1.java
@@ -1,23 +1,48 @@
 public class Review1 {
 
+	public static void main(String[] args) {
+		q1();
+		q2();
+		q3();
+		q4();
+		q5();
+	}
+
 	public static void q1() {
-		//Write question 1 code here
-	}
+        //Write question 1 code here
+        System.out.println("There once was a man from St. Ives.");
+    }
 
-	public static void q2() {
-		//Write question 2 code here
-	}
 
-	public static void q3() {
-		//Write question 3 code here
-	}
+    public static void q2() {
+        //Write question 2 code here
+        int a = 9;
+        int b = 5;
+        System.out.println(a / b);
+        System.out.println(a % b);
+    }
 
-	public static void q4() {
-		//Write question 4 code here
-	}
 
-	public static void q5() {
-		//Write question 5 code here
-	}
+    public static void q3() {
+        //Write question 3 code here
+        System.out.println("Hello\nHello again");
+    }
+
+
+    public static void q4() {
+        //Write question 4 code here
+        boolean t = true;
+        System.out.println(t);
+    }
+
+
+    public static void q5() {
+        //Write question 5 code here
+        String h = "Hello";
+        String w = "World";
+        System.out.print(h+" ");
+        System.out.println(w);
+    }
+
 
 }
